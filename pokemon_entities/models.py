c18011b9c8bb46ffd46fc88f45a2a4044effef86
AlexWoIf <alexey.barannikov@gmail.com>
2023-11-05 03:33:31 +0300

    add parent field to pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,7 +7,12 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, blank=True, )
     title_jp = models.CharField(max_length=200, blank=True, )
     description = models.TextField(blank=True, )
-    image = models.ImageField(upload_to='pokemons', null=True, blank=True)
+    image = models.ImageField(upload_to='pokemons', null=True, blank=True, )
+    previous_evolution = models.ForeignKey(
+                            'self',
+                            on_delete=models.CASCADE,
+                            null=True,
+                            blank=True, )
 
     def __str__(self):
         return self.title
